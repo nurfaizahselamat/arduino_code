dc22bd0939d1d1b438a3c5f14d71c5942f7489e6
nurfaizahselamat <114842283+nurfaizahselamat@users.noreply.github.com>
2022-10-25 09:49:26 +0800

    Add files via upload

---
+++ mqtt dht11.ino
@@ -0,0 +1,67 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+
+#define ssid "iotclass@unifi"         
+#define password "classiot123"
+#define mqtt_server "broker.hivemq.com"
+
+DHT dht(25, DHT11);
+char temp[10];
+char humi[10];
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void connectToWiFi(){
+  Serial.print("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32nurfaizah")) {
+      Serial.println("connected");
+    } else {
+      Serial.println(" trying again");
+      delay(1000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectToWiFi();
+  client.setServer(mqtt_server, 1883);
+  dht.begin();
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  delay(1000);
+  float h = dht.readHumidity();
+  dtostrf(h, 4, 2, humi);
+  float t = dht.readTemperature();
+  dtostrf(t, 4, 2, temp);
+    Serial.print("Suhu = ");
+    Serial.print(temp);
+    Serial.print("°C");
+    client.publish("nurfaizah/temp", temp);
+    Serial.print(" || ");
+    Serial.print("Kelembapan = ");
+    Serial.print(humi);
+    Serial.println("%");
+    client.publish("nurfaizah/humi", humi);
+}
